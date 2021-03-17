724c714f4d2a655129413fe8d9d1b5ebfbf78b2b
Eol Nuha <eol.nuha22@gmail.com>
2021-03-17 15:11:02 +0100

    Added bootstrap

--- datadump/views.py
+++ datadump/views.py
@@ -19,7 +19,7 @@ def post_list(request, tag_slug=None):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                             status='published',
+                             status='active',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
