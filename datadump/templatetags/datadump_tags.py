a4ed48195ff580f306701161c327b8c21280a033
Eol Nuha <eol.nuha22@gmail.com>
2021-03-28 09:34:09 +0100

    Fixed the problems based on review.
    Couldn't get inclusion_tags to work so they are all simple_tags.

--- datadump/templatetags/datadump_tags.py
+++ datadump/templatetags/datadump_tags.py
@@ -1,32 +1,21 @@
 from django import template
+from django.db.models import Count
 from datadump.models import Post
 
 register = template.Library()
 
 
-def get_latest_posts(arg):
-    value = Post.objects.all()[:arg]
-    return value
+@register.simple_tag
+def get_latest_posts(count=5):
+    return Post.published.all()[:count]
 
 
-def get_most_tags(arg):
-    value = Post.tags.most_common()[:arg]
-    return value
+@register.simple_tag
+def get_most_tags(count=5):
+    return Post.tags.most_common()[:count]
 
 
-def get_most_comments(arg):
-    post_list = Post.objects.all().order_by('-comments')
-    post_counter = {}
-    for post in post_list:
-        if post in post_counter:
-            post_counter[post] += 1
-        else:
-            post_counter[post] = 1
-    popular_posts = sorted(post_counter, key=post_counter.get, reverse=True)
-    top = popular_posts[:arg]
-    return top
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
-
-register.filter('get_latest_posts', get_latest_posts)
-register.filter('get_most_tags', get_most_tags)
-register.filter('get_most_comments', get_most_comments)
--- datadump/views.py
+++ datadump/views.py
@@ -30,7 +30,7 @@ def post_list(request, tag_slug=None):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                             status='active',
+                             status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
