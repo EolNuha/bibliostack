50a6f801a4ed7989b2dec6179bd0fcdbe8aff0aa
Eol Nuha <eol.nuha22@gmail.com>
2021-04-01 15:15:01 +0100

    Added login

--- bibliostack/urls.py
+++ bibliostack/urls.py
@@ -2,22 +2,22 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
+from accounts import views as acc_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('datadump/', include('datadump.urls', namespace='datadump')),
+    path('register/', acc_views.register, name='register'),
+    path('profile/', acc_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
