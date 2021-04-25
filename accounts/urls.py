5a5359219866db526eac394e6c4c20a81dfbfb10
Gentian Demaj <gentian@starlabspro.com>
2021-04-25 16:25:58 +0200

    refactoring: small tweaks

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+# app_name = 'account'
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
